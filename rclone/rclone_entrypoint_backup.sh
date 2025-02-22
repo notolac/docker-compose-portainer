809c0369d702d14e8379f578574578da3a108999
notolac <carlos@notolac.org>
2025-02-22 15:05:16 +0100

    refactor rclone entrypoint scripts for backup and restore functionality

---
+++ rclone/rclone_entrypoint_backup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Bucle infinito para ejecutar la sincronización cada 24 horas
+while true; do
+  
+  # Sincronizar la carpeta local /media/multimedia con Google Drive
+  # Esto sube cualquier cambio desde el almacenamiento local a la nube
+  rclone -vv sync /media/multimedia chiguire-bot-gdrive:/multimedia
+  
+  # Sincronizar la carpeta local /media/Plex con Google Drive
+  # Se asegura de que la versión en Google Drive sea un reflejo exacto de la local
+  rclone -vv sync /media/Plex chiguire-bot-gdrive:/Plex
+  
+  # Esperar 24 horas antes de volver a ejecutar la sincronización
+  # 86400 segundos = 24 horas
+  sleep 86400
+
+done
---
+++ rclone/rclone_entrypoint_restore.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+while true; do
+  # Sincronizar desde Google Drive a multimedia local
+  rclone -vv sync chiguire-bot-gdrive:/multimedia /media/multimedia
+  # Sincronizar desde Google Drive a Plex local
+  rclone -vv sync chiguire-bot-gdrive:/Plex /media/Plex
+  # Esperar 24 horas antes de la siguiente sincronización
+  sleep 86400  # 86400 segundos = 24 horas
+done
