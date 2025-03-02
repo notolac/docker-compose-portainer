6ca50f95d973856410d889254409a5f91e82de56
notolac <carlos@notolac.org>
2025-03-02 21:33:12 +0000

    update rclone entrypoint script to use 'copy' instead of 'sync' for Google Drive downloads

--- rclone/rclone_entrypoint_restore.sh
+++ rclone/rclone_entrypoint_restore.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 while true; do
-  # Sincronizar desde Google Drive a multimedia local
-  rclone -vv sync chiguire-bot-gdrive:/multimedia /data/multimedia
-  # Sincronizar desde Google Drive a Plex local
-  rclone -vv sync chiguire-bot-gdrive:/Plex /data/Plex
+  # Descargar desde Google Drive a multimedia local (sin borrar archivos)
+  rclone -vv copy chiguire-bot-gdrive:/multimedia /data/multimedia
+  
+  # Descargar desde Google Drive a Plex local (sin borrar archivos)
+  rclone -vv copy chiguire-bot-gdrive:/Plex /data/Plex
+  
   # Esperar 24 horas antes de la siguiente sincronización
   sleep 86400  # 86400 segundos = 24 horas
 done
