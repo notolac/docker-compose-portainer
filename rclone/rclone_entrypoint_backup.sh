56248fa59f1358e95f1be4d927be1931b868b329
notolac <carlos@notolac.org>
2025-04-17 21:32:55 +0000

    update rclone entrypoint scripts to change local storage paths for synchronization with Google Drive

--- rclone/rclone_entrypoint_backup.sh
+++ rclone/rclone_entrypoint_backup.sh
@@ -3,13 +3,13 @@
 # Bucle infinito para ejecutar la sincronización cada 24 horas
 while true; do
   
-  # Sincronizar la carpeta local /media/multimedia con Google Drive
+  # Sincronizar la carpeta local /storage_hdd/multimedia con Google Drive
   # Esto sube cualquier cambio desde el almacenamiento local a la nube
-  rclone -vv sync /data/multimedia chiguire-bot-gdrive:/multimedia
+  rclone -vv sync /storage_hdd/multimedia chiguire-bot-gdrive:/multimedia
   
   # Sincronizar la carpeta local /media/Plex con Google Drive
   # Se asegura de que la versión en Google Drive sea un reflejo exacto de la local
-  rclone -vv sync /data/Plex chiguire-bot-gdrive:/Plex
+  rclone -vv sync /media/Plex chiguire-bot-gdrive:/Plex
   
   # Esperar 24 horas antes de volver a ejecutar la sincronización
   # 86400 segundos = 24 horas
--- rclone/rclone_entrypoint_restore.sh
+++ rclone/rclone_entrypoint_restore.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 while true; do
   # Descargar desde Google Drive a multimedia local (sin borrar archivos)
-  rclone -vv copy chiguire-bot-gdrive:/multimedia /data/multimedia
+  rclone -vv copy chiguire-bot-gdrive:/multimedia /storage_hdd/multimedia
   
   # Descargar desde Google Drive a Plex local (sin borrar archivos)
-  rclone -vv copy chiguire-bot-gdrive:/Plex /data/Plex
+  # Comentado temporalmente ya que no es necesario en este momento
+  rclone -vv copy chiguire-bot-gdrive:/Plex /media/Plex
   
   # Esperar 24 horas antes de la siguiente sincronización
   sleep 86400  # 86400 segundos = 24 horas
